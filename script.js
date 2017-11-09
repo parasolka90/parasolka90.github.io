13c2064b11be577728a06b3d28c7bf397acd19c4
parasolka90 <parasolka90@gmail.com>
2017-11-09 18:36:17 +0100

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://infinite-fortress-90029.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1/';
+  const trelloApiRoot = 'https://infinite-fortress-90029.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
